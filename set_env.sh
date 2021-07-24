f9ba3c9706b153a904d19925c65fd780833a0dde
hugo.machado <hugo.machado@m4u.com.br>
2021-07-24 14:35:05 -0300

    init project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Hdbpdm5031
+export POSTGRES_HOST=udagram-microservice.ccgfl4rjc3w1.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-microservice
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
